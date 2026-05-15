54ca4db6c24ec3f8cd267b26cb3f5a8be4fee8bf
BohaniLucas <malulekelucas04@gmail.com>
2026-05-15 13:14:30 +0200

    Updated

--- src/server/routes/adminRoute.js
+++ src/server/routes/adminRoute.js
@@ -15,12 +15,12 @@ const router = express.Router();
 
 router.get("/admin/dashboard",verifyToken,requireAdmin,adminDashBoard )
 
-router.get(   "/admin/organisations",              verifyToken, requireAdmin, getAllOrganisations);
-router.get(   "/admin/organisations/:orgId",       verifyToken, requireAdmin, getOrganisationById);
-router.patch( "/admin/organisations/:orgId/approve", verifyToken, requireAdmin, approveOrganisation);
-router.patch( "/admin/organisations/:orgId/reject",  verifyToken, requireAdmin, rejectOrganisation);
-router.delete("/admin/organisations/:orgId",       verifyToken, requireAdmin, deleteOrganisation);
-router.get(   "/admin/students",        verifyToken, requireAdmin, getAllStudents);
+router.get( "/admin/organisations",verifyToken, requireAdmin, getAllOrganisations);
+router.get( "/admin/organisations/:orgId",verifyToken, requireAdmin, getOrganisationById);
+router.patch( "/admin/organisations/:orgId/approve",verifyToken, requireAdmin, approveOrganisation);
+router.patch( "/admin/organisations/:orgId/reject", verifyToken, requireAdmin, rejectOrganisation);
+router.delete("/admin/organisations/:orgId",verifyToken, requireAdmin, deleteOrganisation);
+router.get("/admin/students",verifyToken, requireAdmin, getAllStudents);
 router.delete("/admin/students/:stuId", verifyToken, requireAdmin, deleteStudent);
 
 export default router;
