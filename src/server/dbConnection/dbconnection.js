53a23c21a6aa1c753b87ada77b7d0f10f49903b4
Brandon101-git <khulisomafatshe@gmail.com>
2026-06-03 11:14:04 +0200

    New

--- src/server/dbConnection/dbconnection.js
+++ src/server/dbConnection/dbconnection.js
@@ -4,19 +4,7 @@ dotenv.config();
 import sql from "mssql";
 
 const config = {
-<<<<<<< HEAD
     connectionString:`Driver={ODBC Driver 18 for SQL Server};Server=${process.env.BRANDON_SERVER_NAME};Database=${process.env.DATABASE_NAME};Trusted_Connection=yes;Encrypt=no;TrustServerCertificate=yes`
-=======
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.LUCAS_SERVER_NAME,
-    database: process.env.DATABASE_NAME,
-
-    options: {
-        encrypt: true,
-        trustServerCertificate: false
-    }
->>>>>>> dev
 };
 
 export const connectToDB = async () => {
